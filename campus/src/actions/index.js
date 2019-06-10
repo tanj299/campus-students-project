8ba6d871d92bb5fdcdff664acf3242cc7468cf03
Topgyal Gurung <topgyaltsering3@gmail.com>
2019-06-10 14:15:10 -0400

    add redux file

---
+++ campus/src/actions/index.js
@@ -0,0 +1,10 @@
+ // actions/index.js
+// Namespace actions
+
+//export const INCREMENT = 'counter/INCREMENT';
+
+//const FOO='FOO'
+// function foo(num){
+//     type:foo,
+//     num
+// }
---
+++ campus/src/reducers/index.js
@@ -0,0 +1,23 @@
+//reducers/index.js
+
+// import {INCREMENT} from '../actions';
+
+// const initialState = 0;
+
+// export default (state = initialState, action) => {
+//   switch (action.type) { 
+//     case 'ADD_POST':
+//         return Object.assign({},state,{
+//           post:[
+//             state.posts,
+//             action.data]});
+//         case INCREMENT:
+//             return state+num;
+//         default:
+//             return state;
+//           }
+//         };
+
+//         ReactDOM.render(<Provider store={store}>
+//             <AppName/>
+//           </Provider>)
